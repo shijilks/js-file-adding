b52a94960522858fed662ae21008f8dc55f9f648
shijil <shijilks2000@gmail.com>
2023-12-02 11:37:50 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,11 @@
+//Get Element BY Name
+var a = document.getElementsByName("text")[0];
+var b = document.getElementById("head");
+
+function message(){
+    b.innerHTML ="welcome to js file :"+ a.value;
+}
+
+
+
+
